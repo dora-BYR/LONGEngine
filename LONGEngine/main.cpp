1ee90da2f47cbaf7f3ce4e927a60b3436c439aee
龙辉 <longdora@juhuiwan.cn>
2016-04-06 23:53:29 +0800

    project initialized

---
+++ LONGEngine/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  LONGEngine
+//
+//  Created by 龙辉 on 16/4/6.
+//  Copyright (c) 2016 yuemo studio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+
+    return 0;
+}
